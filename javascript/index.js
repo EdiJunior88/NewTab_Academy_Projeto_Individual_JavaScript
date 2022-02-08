cb03e54b956200ec6b08fe023c2ea1c9ab1b5176
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-07 22:19:30 -0300

    Primeira etapa de Inclusão dos dados capturados pelo formulário e adicionados no extrato

--- javascript/index.js
+++ javascript/index.js
@@ -2,34 +2,44 @@
 //Botão validar os campos (formulários)
 
 const botaoValidar = () => {
-  const mercadoria = document.querySelector("#mercadoria");
-  const valor = document.querySelector("#valor");
+  let mercadoria = document.querySelector("#mercadoria");
+  let valor = document.querySelector("#valor");
 
   if (mercadoria.value == "" || valor.value == "") {
     alert("Preencha os dados");
   }
 }
 
-const botao = document.querySelector(".button");
+let botao = document.querySelector(".button");
 botao.addEventListener("click", botaoValidar, true);
 
 
 // Inclusão dos dados capturados pelo formulario e adicionados no extrato
 
-extrato = [
+let extrato = [
   {
     simbolo: "+",
-    mercadoria: "biscoito",
-    valor: 12.00
-  },
-  {
-    simbolo: "-",
-    mercadoria: "bolo",
-    valor: 8.00
+    mercadoria: "Biscoito",
+    valor: 20.42
   },
   {
     simbolo: "+",
-    mercadoria: "salame",
-    valor: 20.00
-  }
+    mercadoria: "Pão Doce",
+    valor: 10.11
+  },
+  {
+    simbolo: "+",
+    mercadoria: "Bolo de Fubá",
+    valor: 60.65
+  },
 ]
+
+for (lista in extrato.reverse()) {
+  document.querySelector(".container_tabela").insertAdjacentHTML('afterend', `
+  <div class="container_tabela">
+    <div class="tabela_corpo">${extrato[lista].simbolo}</div>
+    <div class="tabela_corpo">${extrato[lista].mercadoria}</div>
+    <div class="tabela_corpo">${extrato[lista].valor}</div>
+  </div>
+  `)
+}
