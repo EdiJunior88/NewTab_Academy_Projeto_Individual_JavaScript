04ef9cdedea513f4d9e8836c30b1d13dcfdbac73
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-17 11:00:45 -0300

    Função de validação dos formulários e crianção de um event no formulario HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,21 @@
 
+//Função de validação dos formulários
+function validacao(event) {
+  event.preventDefault();
+
+  let mercadoriaFormulario = document.getElementById("mercadoria").value;
+  let valorFormulario = document.getElementById("valor").value;
+
+  if(mercadoriaFormulario == "") {
+    alert("Preencha o nome");
+    document.getElementById("mercadoria").focus();
+    return false;
+  }
+
+  if(valorFormulario == "") {
+    alert("Preencha o valor");
+    document.getElementById("valor").focus();
+    return false;
+  }
+}
 
