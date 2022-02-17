73ef9c16d802e34ecf74e22220c7234509b31323
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-16 22:23:16 -0300

    Commit do zero

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,2 @@
 
-let extrato = [
-  { mercadoria: "Pão doce", valor: "50,45"},
-  { mercadoria: "Mingau", valor: "2,00"},
-  { mercadoria: "Farinha", valor: "3,20"},
-  { mercadoria: "Cuscuz", valor: "10,03"}
-];
 
-for (lista in extrato) {
-  document.querySelector("table.container_secao_2 tbody").innerHTML += `
-  <tr class="container_tabela">
-    <td class="tabela_titulo_2">${ extrato[lista].mercadoria }</div>
-    <td class="tabela_titulo_2">${ extrato[lista].valor }</div>
-  </tr>
-  `
-}
