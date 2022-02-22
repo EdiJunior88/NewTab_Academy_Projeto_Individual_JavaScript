03557155882930d10938443b3b7099584e9add89
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-22 12:42:18 -0300

    ajuste fino no JavaScript

--- javascript/index.js
+++ javascript/index.js
@@ -71,7 +71,7 @@ function totalExtrato() {
   }
 
   if (extrato.length > 0) {
-    document.querySelector("#tabela tfoot").innerHTML += `
+    document.querySelector("#tabela tfoot").innerHTML = `
       <tr class="container_tabela_4">
         <td class="tabela_rodape">Total</td>
         <td class="tabela_rodape">${formatter.format(total)}</td>
@@ -127,17 +127,6 @@ function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){
   return false;  
 }  
 
-/* function formatarMoeda(z) {
-  v = z.value;
-  v=v.replace(/\D/g,"") // permite digitar apenas numero
-  v=v.replace(/(\d{1})(\d{17})$/,"$1.$2") // coloca ponto antes dos ultimos digitos
-  v=v.replace(/(\d{1})(\d{13})$/,"$1.$2") // coloca ponto antes dos ultimos 13 digitos
-  v=v.replace(/(\d{1})(\d{10})$/,"$1.$2") // coloca ponto antes dos ultimos 10 digitos
-  v=v.replace(/(\d{1})(\d{7})$/,"$1$2") // coloca ponto antes dos ultimos 7 digitos
-  v=v.replace(/(\d{1})(\d{5})$/,"$1.$2") // coloca ponto antes dos ultimos 5 digitos
-  v=v.replace(/(\d{1})(\d{1,2})$/,"$1,$2") // coloca virgula antes dos ultimos 2 digitos
-  z.value = v;
-} */
 
 //Função para deletar os dados do LocalStorage
 //Se não houver nenhuma transação cadastrada aparece um alert
