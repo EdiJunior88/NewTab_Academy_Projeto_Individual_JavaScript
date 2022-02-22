a8ea1bd05a634f175d7e5d8d4fe608801deff8df
EdiJunior88 <er.moura88@yahoo.com.br>
2022-02-22 12:25:54 -0300

    Atualização da Máscara no HTML e JS ajustesno alert() quando aperta o botão cancelar

--- javascript/index.js
+++ javascript/index.js
@@ -86,32 +86,73 @@ function totalExtrato() {
 }
 
 //Função máscara monetária para o padrão BR
-function formatarMoeda(z) {
+function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e){  
+  var sep = 0;  
+  var key = '';  
+  var i = j = 0;  
+  var len = len2 = 0;  
+  var strCheck = '0123456789';  
+  var aux = aux2 = '';  
+  var whichCode = (window.Event) ? e.which : e.keyCode;  
+  if (whichCode == 13 || whichCode == 8) return true;  
+  key = String.fromCharCode(whichCode); // Valor para o código da Chave  
+  if (strCheck.indexOf(key) == -1) return false; // Chave inválida  
+  len = objTextBox.value.length;  
+  for(i = 0; i < len; i++)  
+      if ((objTextBox.value.charAt(i) != '0') && (objTextBox.value.charAt(i) != SeparadorDecimal)) break;  
+  aux = '';  
+  for(; i < len; i++)  
+      if (strCheck.indexOf(objTextBox.value.charAt(i))!=-1) aux += objTextBox.value.charAt(i);  
+  aux += key;  
+  len = aux.length;  
+  if (len == 0) objTextBox.value = '';  
+  if (len == 1) objTextBox.value = '0'+ SeparadorDecimal + '0' + aux;  
+  if (len == 2) objTextBox.value = '0'+ SeparadorDecimal + aux;  
+  if (len > 2) {  
+      aux2 = '';  
+      for (j = 0, i = len - 3; i >= 0; i--) {  
+          if (j == 3) {  
+              aux2 += SeparadorMilesimo;  
+              j = 0;  
+          }  
+          aux2 += aux.charAt(i);  
+          j++;  
+      }  
+      objTextBox.value = '';  
+      len2 = aux2.length;  
+      for (i = len2 - 1; i >= 0; i--)  
+      objTextBox.value += aux2.charAt(i);  
+      objTextBox.value += SeparadorDecimal + aux.substr(len - 2, len);  
+  }  
+  return false;  
+}  
+
+/* function formatarMoeda(z) {
   v = z.value;
   v=v.replace(/\D/g,"") // permite digitar apenas numero
   v=v.replace(/(\d{1})(\d{17})$/,"$1.$2") // coloca ponto antes dos ultimos digitos
   v=v.replace(/(\d{1})(\d{13})$/,"$1.$2") // coloca ponto antes dos ultimos 13 digitos
   v=v.replace(/(\d{1})(\d{10})$/,"$1.$2") // coloca ponto antes dos ultimos 10 digitos
-  v=v.replace(/(\d{1})(\d{7})$/,"$1.$2") // coloca ponto antes dos ultimos 7 digitos
+  v=v.replace(/(\d{1})(\d{7})$/,"$1$2") // coloca ponto antes dos ultimos 7 digitos
   v=v.replace(/(\d{1})(\d{5})$/,"$1.$2") // coloca ponto antes dos ultimos 5 digitos
   v=v.replace(/(\d{1})(\d{1,2})$/,"$1,$2") // coloca virgula antes dos ultimos 2 digitos
   z.value = v;
-}
+} */
 
 //Função para deletar os dados do LocalStorage
 //Se não houver nenhuma transação cadastrada aparece um alert
 //Se houver transação cadastrada a função continua até deletar os dados
 function deletaLocalStorage() {
   if(extrato.length <= 0) {
-      alert("Nenhum registro de transação")
+      alert("Nenhum registro de transação");
     } else {
       let caixaTexto = confirm("Deseja excluir as transações?");
     
-      if(caixaTexto = true) {
+      if(caixaTexto == true) {
         localStorage.clear();
         alert("Transações excluídas");
       } else {
-        alert("Exclusões cancelada");
+        alert("Exclusões canceladas");
       }
     }
     extratoHTML();
